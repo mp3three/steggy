141cfc80c55b128c59fa6726be0e3f342992f0d8
Cameron <cameron@form.io>
2021-03-26 18:25:30 -0500

    WIP: generator

---
+++ tools/generators/microservice/files/main.ts
@@ -0,0 +1,21 @@
+import { Logger } from '@automagical/logger';
+import { NestFactory } from '@nestjs/core';
+import cors from 'cors';
+import * as helmet from 'helmet';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const prefix = 'api-server';
+  const logger = Logger.forNest(prefix);
+
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
+  app.setGlobalPrefix(prefix);
+  // app.use(cors(), helmet());
+  await app.listen(process.env.PORT, () => {
+    logger.log(`Listening on ${process.env.PORT}`);
+  });
+}
+
+bootstrap();
