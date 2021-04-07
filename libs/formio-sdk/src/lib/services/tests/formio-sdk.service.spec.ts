e0daebf85d3c78b845e5a627852087fe8f72ed51
Cameron Contour <github@ccontour.com>
2021-04-07 13:54:51 -0500

    SDK tests

--- libs/formio-sdk/src/lib/services/tests/formio-sdk.service.spec.ts
+++ libs/formio-sdk/src/lib/services/tests/formio-sdk.service.spec.ts
@@ -1,17 +1,35 @@
 import { Test } from '@nestjs/testing';
 import { FormioSdkService } from '../formio-sdk.service';
 import * as faker from 'faker';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Fetch } from '@automagical/fetch';
 
 describe('formio-sdk', () => {
   let formioSdkService: FormioSdkService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: [
+            () => {
+              return {
+                libs: {
+                  'formio-sdk': {
+                    PORTAL_BASE_URL: 'FOO',
+                  },
+                },
+              };
+            },
+          ],
+        }),
+      ],
       providers: [FormioSdkService, ConfigService],
     }).compile();
 
     formioSdkService = moduleRef.get(FormioSdkService);
+
+    await formioSdkService.onModuleInit();
   });
 
   describe('byId', () => {
@@ -22,6 +40,16 @@ describe('formio-sdk', () => {
     it('should return _id if type is obj', () => {
       const str = faker.datatype.uuid();
       expect(formioSdkService.id({ _id: str })).toBe(str);
+    });
+  });
+
+  describe('fetch', () => {
+    afterEach(() => {});
+
+    it('should call Fetch.fetch', () => {
+      jest.spyOn(Fetch, 'fetch').mockImplementation(() => null);
+      formioSdkService.fetch({});
+      expect(Fetch.fetch).toHaveBeenCalled();
     });
   });
 });
