50c4fd01b9bc42503685bdd43d27b1c014c4c8cf
Cameron Contour <35661840+ccontour@users.noreply.github.com>
2021-05-02 16:30:49 -0500

    File cleanup (#24)
    * Delete Access.dto.ts
    
    * Delete Action.dto.ts
    
    * Delete Base.dto.ts
    
    * Delete Form.dto.ts
    
    * Delete Project.dto.ts
    
    * Delete Resource.dto.ts
    
    * Delete Role.dto.ts
    
    * Delete Schema.dto.ts
    
    * Delete Session.dto.ts
    
    * Delete Submission.dto.ts
    
    * Delete Tag.dto.ts
    
    * Delete Token.dto.ts
    
    * Delete Key.dto.ts
    
    * Delete Options.dto.ts
    
    * Delete Report.dto.ts
    
    * Delete Usage.dto.ts
    
    * Delete HTTP.ts
    
    * Delete HTTP.ts
    
    * Delete Metadata.ts
    
    * Delete Token.schema.ts
    
    * Delete Tag.schema.ts
    
    * Delete Submission.schema.ts
    
    * Delete Session.schema.ts
    
    * Delete Schema.schema.ts
    
    * Delete Role.schema.ts
    
    * Delete Project.schema.ts
    
    * Delete Action.schema.ts
    
    * Delete Form.schema.ts

--- libs/formio-sdk/src/typings/Metadata.ts
+++
@@ -1,9 +0,0 @@
-import { LicenseScopes } from '@automagical/contracts/licenses';
-import { SetMetadata } from '@nestjs/common';
-
-export enum MetadataTypes {
-  getUser = 'getUser',
-}
-
-export const UseUtilization = (type: LicenseScopes): unknown =>
-  SetMetadata('licenseUtilization', type);
