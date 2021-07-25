7609867b6b945a13889db650e73f7728e5ee7cb3
Cameron Contour <github@ccontour.com>
2021-07-25 15:23:18 -0500

    nrwl workspace upgrades

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/libs/home-assistant',
-    '<rootDir>/apps/home-controller',
-    '<rootDir>/libs/utilities',
-    '<rootDir>/libs/testing',
-    '<rootDir>/libs/formio-sdk',
-    '<rootDir>/libs/fetch',
-    '<rootDir>/libs/persistence',
-    '<rootDir>/libs/server',
-    '<rootDir>/apps/living-docs',
-    '<rootDir>/apps/devtools',
-    '<rootDir>/libs/custom',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: [...getJestProjects()] };
