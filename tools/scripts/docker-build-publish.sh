2a3efa8e1d55d3a23e92432d40f1941342c7d1bd
Cameron Contour <github@ccontour.com>
2022-01-05 18:06:30 -0600

    shellcheck

--- tools/scripts/docker-build-publish.sh
+++ tools/scripts/docker-build-publish.sh
@@ -1,26 +1,27 @@
+#!/bin/bash
+
 IMAGE=$1
 DIR=$2
-if [ -z $DIR ]
+if [ -z "$DIR" ]
 then
   DIR=$IMAGE
 fi
 # BUILD
-tools/scripts/docker-build.sh $IMAGE $DIR
-PUBLISHER=$(cat package.json | jq .publisher | xargs)
+tools/scripts/docker-build.sh "$IMAGE" "$DIR"
+PUBLISHER=$(jq .publisher < package.json | xargs)
 
-VERSION=$(cat "apps/$DIR/package.json" | jq .version | xargs)
-TAGS=$(npx ts-node tools/scripts/create-tags.js $VERSION)
-IMAGE=$(echo "$PUBLISHER/$IMAGE")
-LATEST=$(echo "latest")
+VERSION=$(jq .version < "apps/$DIR/package.json" | xargs)
+TAGS=$(npx ts-node tools/scripts/create-tags.js "$VERSION")
+IMAGE="$PUBLISHER/$IMAGE"
 
 for TAG in $TAGS
 do
-  if [ "$TAG" != "$LATEST" ]; then
+  if [ "$TAG" != "latest" ]; then
     COMMAND="docker tag $IMAGE:latest $IMAGE:$TAG"
-    echo $COMMAND
-    echo $COMMAND | sh
+    echo "$COMMAND"
+    echo "$COMMAND" | sh
   fi
   COMMAND="docker push $IMAGE:$TAG"
-  echo $COMMAND
-  echo $COMMAND | sh
+  echo "$COMMAND"
+  echo "$COMMAND" | sh
 done
--- tools/scripts/publish-pipeline.sh
+++ tools/scripts/publish-pipeline.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 npx figlet-cli --font="DOS Rebel" "Build Docker"
 npx nx affected --target=build-docker  --parallel=1
 npx figlet-cli --font="DOS Rebel" "NPM Publish"
