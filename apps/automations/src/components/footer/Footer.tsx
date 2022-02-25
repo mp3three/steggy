6847d19700a2620d202b4bb57758a4cef31ae7bd
Cameron Contour <github@ccontour.com>
2022-02-25 15:15:51 -0600

    0.6.3

--- apps/automations/src/components/footer/Footer.tsx
+++ apps/automations/src/components/footer/Footer.tsx
@@ -8,10 +8,7 @@ export class Foot extends React.Component {
   override render() {
     return (
       <Footer style={{ textAlign: 'center' }}>
-        <Link
-          href="https://git.programmable.casa/cameron/automagical"
-          target="_blank"
-        >
+        <Link href="https://github.com/ccontour/automagical" target="_blank">
           @automagical
         </Link>
       </Footer>
