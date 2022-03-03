dfc5b8358e14ffe809ac200e5c8d1dd458ab17b8
Cameron Contour <github@ccontour.com>
2022-03-02 21:11:25 -0600

    Basic operation works

--- libs/tty/src/services/meta/application-manager.service.ts
+++ libs/tty/src/services/meta/application-manager.service.ts
@@ -1,5 +1,14 @@
+import { InjectConfig } from '@automagical/boilerplate';
+import { is } from '@automagical/utilities';
 import { Injectable } from '@nestjs/common';
+import chalk from 'chalk';
+import figlet, { Fonts } from 'figlet';
 
+import {
+  DEFAULT_HEADER_FONT,
+  HEADER_COLOR,
+  SECONDARY_HEADER_FONT,
+} from '../../config';
 import { ApplicationStackProvider, iStackProvider } from '../../contracts';
 import { iComponent } from '../../decorators';
 import { ComponentExplorerService } from '../explorers';
@@ -12,12 +21,15 @@ import { ScreenService } from './screen.service';
 @ApplicationStackProvider()
 export class ApplicationManagerService implements iStackProvider {
   constructor(
+    @InjectConfig(HEADER_COLOR) private readonly color: string,
+    @InjectConfig(DEFAULT_HEADER_FONT) private readonly primaryFont: Fonts,
+    @InjectConfig(SECONDARY_HEADER_FONT) private readonly secondaryFont: Fonts,
     private readonly componentExplorer: ComponentExplorerService,
     private readonly layoutManager: LayoutManagerService,
     private readonly screenService: ScreenService,
   ) {}
-
   private activeApplication: iComponent;
+  private header: string;
 
   public async activate<CONFIG, VALUE>(
     name: string,
@@ -50,9 +62,34 @@ export class ApplicationManagerService implements iStackProvider {
     return this.activeApplication;
   }
 
-  public setHeader(main: string, secondary?: string): void {
+  public setHeader(primary: string, secondary = ''): void {
     this.screenService.clear();
-    // this.layoutManager.setHeader(main, secondary);
+    primary = figlet.textSync(primary, {
+      font: this.primaryFont,
+    });
+    this.screenService.print(
+      `\n` +
+        chalk
+          .cyan(primary)
+          .split(`\n`)
+          .map(i => `  ${i}`)
+          .join(`\n`),
+    );
+    if (is.empty(secondary)) {
+      return;
+    }
+    secondary = figlet.textSync(secondary, {
+      font: this.secondaryFont,
+    });
+    this.screenService.print(
+      chalk
+        .magenta(secondary)
+        .split(`\n`)
+        .map(i => `  ${i}`)
+        .join(`\n`),
+    );
+
+    //  header.split(`\n`).pop().length;
   }
 
   private reset(): void {
