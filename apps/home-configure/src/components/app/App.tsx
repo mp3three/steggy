294681be1a73fbf757078753045406a888b3d316
Cameron Contour <github@ccontour.com>
2022-04-27 20:51:14 -0500

    App folder => functional components

--- apps/home-configure/src/components/app/App.tsx
+++ apps/home-configure/src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import { is } from '@steggy/utilities';
 import { Layout, Typography } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
@@ -18,69 +18,60 @@ import { ApplicationMenu } from './ApplicationMenu';
 const { Header, Sider, Content } = Layout;
 
 type tState = {
-  ADMIN_KEY: string;
-  BASE_URL: string;
-  collapsed: boolean;
+  ADMIN_KEY?: string;
+  BASE_URL?: string;
+  collapsed?: boolean;
 };
 
-export class App extends React.Component {
-  override state = {
+export function App() {
+  const [state, setState] = useState<tState>({
     ADMIN_KEY: localStorage.getItem(ADMIN_KEY),
     BASE_URL: localStorage.getItem(BASE_URL),
     collapsed: false,
-  } as tState;
-
-  override render() {
-    const { collapsed } = this.state;
-    return (
-      <Provider store={store}>
-        {/* eslint-disable-next-line spellcheck/spell-checker */}
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sider
-            collapsible
-            collapsed={collapsed}
-            onCollapse={this.onCollapse.bind(this)}
-          >
-            <ApplicationMenu />
-          </Sider>
-          <Layout>
-            <Header>
-              <Typography.Title level={2} style={{ padding: '8px' }}>
-                Automation Controller
-              </Typography.Title>
-            </Header>
-            <Content>
-              {is.empty(this.state.ADMIN_KEY) ? (
-                <SettingsPage
-                  onConnectionUpdate={update => this.setState(update)}
-                />
-              ) : (
-                <Switch>
-                  <Route path="/people" component={PeoplePage} />
-                  <Route path="/entities" component={EntityPage} />
-                  <Route path="/routines" component={RoutinePage} />
-                  <Route path="/rooms" component={RoomPage} />
-                  <Route path="/groups" component={GroupPage} />
-                  <Route path="/settings" component={SettingsPage} />
-                  <Route path="/" component={HomePage} />
-                </Switch>
-              )}
-            </Content>
-            <Layout.Footer style={{ textAlign: 'center' }}>
-              <Typography.Link
-                href="https://github.com/ccontour/steggy"
-                target="_blank"
-              >
-                @steggy
-              </Typography.Link>
-            </Layout.Footer>
-          </Layout>
+  });
+  const { collapsed } = state;
+  return (
+    <Provider store={store}>
+      {/* eslint-disable-next-line spellcheck/spell-checker */}
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={() => setState({ collapsed: true })}
+        >
+          <ApplicationMenu />
+        </Sider>
+        <Layout>
+          <Header>
+            <Typography.Title level={2} style={{ padding: '8px' }}>
+              Automation Controller
+            </Typography.Title>
+          </Header>
+          <Content>
+            {is.empty(state.ADMIN_KEY) ? (
+              <SettingsPage onConnectionUpdate={update => setState(update)} />
+            ) : (
+              <Switch>
+                <Route path="/people" component={PeoplePage} />
+                <Route path="/entities" component={EntityPage} />
+                <Route path="/routines" component={RoutinePage} />
+                <Route path="/rooms" component={RoomPage} />
+                <Route path="/groups" component={GroupPage} />
+                <Route path="/settings" component={SettingsPage} />
+                <Route path="/" component={HomePage} />
+              </Switch>
+            )}
+          </Content>
+          <Layout.Footer style={{ textAlign: 'center' }}>
+            <Typography.Link
+              href="https://github.com/ccontour/steggy"
+              target="_blank"
+            >
+              @steggy
+            </Typography.Link>
+          </Layout.Footer>
         </Layout>
-      </Provider>
-    );
-  }
-
-  private onCollapse(collapsed: boolean) {
-    this.setState({ collapsed });
-  }
+      </Layout>
+    </Provider>
+  );
 }
--- apps/home-configure/src/components/app/ApplicationMenu.tsx
+++ apps/home-configure/src/components/app/ApplicationMenu.tsx
@@ -5,61 +5,59 @@ import { Link } from 'react-router-dom';
 
 import { FD_ICONS, sendRequest } from '../../types';
 
-export class ApplicationMenu extends React.Component {
-  override render() {
-    return (
-      <Menu theme="dark" defaultSelectedKeys={this.getSelected()} mode="inline">
-        {is.empty(sendRequest.ADMIN_KEY) ? undefined : (
-          <>
-            <Menu.Item key="home" icon={FD_ICONS.get('home')}>
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="groups" icon={FD_ICONS.get('groups')}>
-              <Link to="/groups">Groups</Link>
-            </Menu.Item>
-            <Menu.Item key="rooms" icon={FD_ICONS.get('rooms')}>
-              <Link to="/rooms">Rooms</Link>
-            </Menu.Item>
-            <Menu.Item key="people" icon={FD_ICONS.get('people')}>
-              <Link to="/people">People</Link>
-            </Menu.Item>
-            <Menu.Item key="routines" icon={FD_ICONS.get('routines')}>
-              <Link to="/routines">Routines</Link>
-            </Menu.Item>
-            <Menu.Item key="entities" icon={FD_ICONS.get('entities')}>
-              <Link to="/entities">Entities</Link>
-            </Menu.Item>
-          </>
-        )}
-        <Menu.Item key="settings" icon={FD_ICONS.get('settings')}>
-          <Link to="/settings">Settings</Link>
-        </Menu.Item>
-      </Menu>
-    );
+function getSelected(): string[] {
+  if (is.empty(sendRequest.ADMIN_KEY)) {
+    return ['settings'];
   }
+  if (window.location.href.includes('/room')) {
+    return ['rooms'];
+  }
+  if (window.location.href.includes('/people')) {
+    return ['people'];
+  }
+  if (window.location.href.includes('/entities')) {
+    return ['entities'];
+  }
+  if (window.location.href.includes('/groups')) {
+    return ['groups'];
+  }
+  if (window.location.href.includes('/routines')) {
+    return ['routines'];
+  }
+  if (window.location.href.includes('/settings')) {
+    return ['settings'];
+  }
+  return ['home'];
+}
 
-  private getSelected(): string[] {
-    if (is.empty(sendRequest.ADMIN_KEY)) {
-      return ['settings'];
-    }
-    if (window.location.href.includes('/room')) {
-      return ['rooms'];
-    }
-    if (window.location.href.includes('/people')) {
-      return ['people'];
-    }
-    if (window.location.href.includes('/entities')) {
-      return ['entities'];
-    }
-    if (window.location.href.includes('/groups')) {
-      return ['groups'];
-    }
-    if (window.location.href.includes('/routines')) {
-      return ['routines'];
-    }
-    if (window.location.href.includes('/settings')) {
-      return ['settings'];
-    }
-    return ['home'];
-  }
+export function ApplicationMenu() {
+  return (
+    <Menu theme="dark" defaultSelectedKeys={getSelected()} mode="inline">
+      {is.empty(sendRequest.ADMIN_KEY) ? undefined : (
+        <>
+          <Menu.Item key="home" icon={FD_ICONS.get('home')}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="groups" icon={FD_ICONS.get('groups')}>
+            <Link to="/groups">Groups</Link>
+          </Menu.Item>
+          <Menu.Item key="rooms" icon={FD_ICONS.get('rooms')}>
+            <Link to="/rooms">Rooms</Link>
+          </Menu.Item>
+          <Menu.Item key="people" icon={FD_ICONS.get('people')}>
+            <Link to="/people">People</Link>
+          </Menu.Item>
+          <Menu.Item key="routines" icon={FD_ICONS.get('routines')}>
+            <Link to="/routines">Routines</Link>
+          </Menu.Item>
+          <Menu.Item key="entities" icon={FD_ICONS.get('entities')}>
+            <Link to="/entities">Entities</Link>
+          </Menu.Item>
+        </>
+      )}
+      <Menu.Item key="settings" icon={FD_ICONS.get('settings')}>
+        <Link to="/settings">Settings</Link>
+      </Menu.Item>
+    </Menu>
+  );
 }
