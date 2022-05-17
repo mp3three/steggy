01edb0c68cc852b91cbebdd549974d95c682c7ce
Cameron Contour <github@ccontour.com>
2022-05-16 20:07:14 -0500

    External loading confirmed

--- libs/custom-code/src/modules/custom-code.module.ts
+++ libs/custom-code/src/modules/custom-code.module.ts
@@ -1,5 +1,6 @@
 import { DynamicModule } from '@nestjs/common';
 import { LOGGER_LIBRARY } from '@steggy/boilerplate';
+import { is } from '@steggy/utilities';
 import { existsSync } from 'fs';
 import { homedir } from 'os';
 import { join } from 'upath';
@@ -18,21 +19,28 @@ export const CustomCodeModule = (application: symbol): DynamicModule => {
   };
 };
 
+// ? Deliberately using environment variables here
+// This code is intended to be run prior to bootstrapping
+// The general config loader is not available at this time
+
 function load(application: symbol) {
+  // ? Referencing this document
+  // - https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
   const DATA_DIRS = [
-    ...(process.env.XDG_DATA_DIRS ?? '/usr/local/share/:/usr/share/').split(
-      ':',
-    ),
-    join(homedir(), '.local', 'share'),
+    ...(process.env.XDG_DATA_DIRS ?? '/usr/local/share/:/usr/share/')
+      .split(':')
+      .map(i => join(i, application.description)),
+    join(__dirname, 'custom-code'),
+    join(homedir(), '.local', 'share', application.description),
   ];
-  const LOADED_DATA_PATH = DATA_DIRS.find(path =>
-    existsSync(join(path, application.description)),
-  );
-  if (!LOADED_DATA_PATH) {
+  if (!is.empty(process.env.XDG_DATA_HOME)) {
+    DATA_DIRS.push(join(process.env.XDG_DATA_HOME, application.description));
+  }
+  const LOADED_DATA_PATH = DATA_DIRS.find(path => existsSync(path));
+  if (is.empty(LOADED_DATA_PATH)) {
     return;
   }
-  // ! Go out and grab the path and run it BEFORE bootstrapping
-
-  // eslint-disable-next-line security/detect-non-literal-require
+  // Yolo require
+  // Webpack isn't getting involved, and who knows what might happen after this point
   nativeRequire(LOADED_DATA_PATH);
 }
