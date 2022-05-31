9d16a146d1a0d901a9f1b85b21e796ab64034eef
Cameron Contour <github@ccontour.com>
2022-05-31 13:31:07 -0500

    reflect-metadata included as boilerplate lib dep

--- libs/boilerplate/src/index.ts
+++ libs/boilerplate/src/index.ts
@@ -1,3 +1,6 @@
+// This is purely so that builds include it in the package.json output
+import 'reflect-metadata';
+
 export * from './config';
 export * from './contracts';
 export * from './decorators';
